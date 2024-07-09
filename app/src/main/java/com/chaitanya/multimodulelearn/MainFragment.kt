5eceb3c521cc07d6f3c7e7b870e4e03fd696e0eb
Chaitanya-kamra <chaitanyakamra001@gmail.com>
2024-07-09 18:20:37 +0530

    update

--- app/src/main/java/com/chaitanya/multimodulelearn/MainFragment.kt
+++ app/src/main/java/com/chaitanya/multimodulelearn/MainFragment.kt
@@ -1,16 +1,15 @@
-package com.chaitanya.home
+package com.chaitanya.multimodulelearn
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.navigation.NavDeepLinkRequest
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.navigateUp
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,10 +18,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [MainFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class MainFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -40,18 +39,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.atv).setOnClickListener {
-
-            val request = NavDeepLinkRequest.Builder
-                .fromUri("myApp://dashboard".toUri())
-                .build()
-            findNavController().navigate(request)
+        view.apply{
+            findViewById<Button>(R.id.mainBt).setOnClickListener {
+                val request = NavDeepLinkRequest.Builder
+                    .fromUri("myApp://home".toUri())
+                    .build()
+                findNavController().navigate(request)
+            }
         }
+
     }
 
     companion object {
@@ -61,12 +62,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment MainFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            MainFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
